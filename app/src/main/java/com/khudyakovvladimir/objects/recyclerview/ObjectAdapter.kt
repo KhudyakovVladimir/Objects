8ee72ba7aeea6b3d0984306c6ce7289899955e3a
Khudyakov Vladimir <khudyakov.vladimir.developer@list.ru>
2022-12-13 22:13:44 +0300

    xml

--- app/src/main/java/com/khudyakovvladimir/objects/recyclerview/ObjectAdapter.kt
+++ app/src/main/java/com/khudyakovvladimir/objects/recyclerview/ObjectAdapter.kt
@@ -33,7 +33,7 @@ class ObjectAdapter(
             textView2.text = objectEntity.title
 
             if (objectEntity.duty != "") {
-                val unicode = 0x1F692
+                val unicode = 0x1F9EF
                 val textEmoji = String(Character.toChars(unicode))
                 textView3.text = textEmoji
 
